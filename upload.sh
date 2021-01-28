d5d13f877ab620ae29801e63fd2edde4b031f6de
Adrian Riedel <adrian.riedel@outlook.com>
2021-01-28 19:09:10 +0100

    add script for packing and uploading new pack version to S3

---
+++ upload.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+zip -9 -r out/HOTG.zip src/*
+
+CURRENT_VERSION=$(aws s3 ls s3://madzoo.events/packs/ | grep -o "HOTG.*.zip" | sed "s/[^0-9+]//g")
+NEXT_VERSION=$((CURRENT_VERSION + 1))
+
+aws s3 rm s3://madzoo.events/packs/HOTG${CURRENT_VERSION}.zip
+aws s3 cp out/HOTG.zip s3://madzoo.events/packs/HOTG${NEXT_VERSION}.zip
