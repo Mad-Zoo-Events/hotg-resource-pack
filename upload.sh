cbf33c0a3535f247ca8fcab328e724164de3948b
Adrian Riedel <adrian.riedel@outlook.com>
2021-04-21 01:36:47 +0200

    make upload script robust against missing AWS permissions

--- upload.sh
+++ upload.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 rm out/HOTG.zip
 
-cd src
-zip -9 -r ../out/HOTG.zip .
-cd ..
+(cd src && zip -9 -r ../out/HOTG.zip .)
 
 CURRENT_VERSION=$(aws s3 ls s3://madzoo.events/packs/ | grep -o "HOTG.*.zip" | sed "s/[^0-9+]//g")
+
+if [ -z "$CURRENT_VERSION" ]; then
+    echo "failed to get version number... Aborting"
+    exit 1
+fi
+
 NEXT_VERSION=$((CURRENT_VERSION + 1))
 
-aws s3 rm s3://madzoo.events/packs/HOTG${CURRENT_VERSION}.zip
-aws s3 cp out/HOTG.zip s3://madzoo.events/packs/HOTG${NEXT_VERSION}.zip
+aws s3 cp "out/HOTG.zip s3://madzoo.events/packs/HOTG${NEXT_VERSION}.zip"
+aws s3 rm "s3://madzoo.events/packs/HOTG${CURRENT_VERSION}.zip"
