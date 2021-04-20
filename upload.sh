4ba9400afdb6e04691e33b53488db39c08e0a96d
Adrian Riedel <adrian.riedel@outlook.com>
2021-04-21 01:42:25 +0200

    auto-commit changes when running upload script

--- upload.sh
+++ upload.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+if [ -z "$1" ]; then
+    echo "usage:"
+    echo -e "./upload.sh \"your commit message goes here\""
+    exit 1
+fi
+
 rm out/HOTG.zip
 
 (cd src && zip -9 -r ../out/HOTG.zip .)
@@ -14,3 +20,7 @@ NEXT_VERSION=$((CURRENT_VERSION + 1))
 
 aws s3 cp "out/HOTG.zip s3://madzoo.events/packs/HOTG${NEXT_VERSION}.zip"
 aws s3 rm "s3://madzoo.events/packs/HOTG${CURRENT_VERSION}.zip"
+
+git add src/ out/
+git commit -m "$1"
+git push
